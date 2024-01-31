137b3caf8b7d3f975143a6281d3a7c468dd9f72d
Jason Bryer <jason@bryer.org>
2024-01-30 21:56:16 -0500

    Documentation updates.

--- inst/login_demo_simple/app.R
+++ inst/login_demo_simple/app.R
@@ -1,37 +1,20 @@
 library(shiny)
 library(login)
-library(shinyjs)
-
-if(file.exists('config.R')) {
-    source('config.R')
-} else {
-    warning('No configuration file found. Email will not work.')
-}
 
 ###### User Interface ##########################################################
 ui <- fluidPage(
-    useShinyjs(),
-    titlePanel("Shiny Login Template"),
-    div(id = 'login_box',
-        tabsetPanel(
-            id = 'login_panel',
-            tabPanel('Login',
-                     login::login_ui(id = 'login_demo') ),
-            tabPanel('Create Account',
-                     login::new_user_ui(id = 'login_demo') ),
-            tabPanel('Reset Password',
-                     login::reset_password_ui(id = 'login_demo'))
-        )
-    ),
-    logout_ui('login_demo'),
+    titlePanel("Shiny Login Simple Demo"),
+    p("You can login with 'test/test'."),
+    login::login_ui(id = 'login_demo'),
+    login::logout_ui('login_demo'),
     hr(),
     div('Are you logged in? ', textOutput('is_logged_in')),
     div('Username: ', textOutput('username')),
-    is_logged_in(
+    login::is_logged_in(
         id = 'login_demo',
         div("This only shows when you are logged in!")
     ),
-    is_not_logged_in(
+    login::is_not_logged_in(
         id = 'login_demo',
         div("This only shows when you are NOT logged in!")
     )
@@ -41,13 +24,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     USER <- login::login_server(
         id = 'login_demo',
-        db_conn = RSQLite::dbConnect(RSQLite::SQLite(), 'users.sqlite'),
-        reset_password_from_email = reset_password_from_email,
-        reset_password_subject = 'Reset password',
-        email_host = email_host,
-        email_port = email_port,
-        email_username = email_username,
-        email_password = email_password
+        db_conn = RSQLite::dbConnect(RSQLite::SQLite(), 'users.sqlite')
     )
 
     observeEvent(USER$logged_in, {
